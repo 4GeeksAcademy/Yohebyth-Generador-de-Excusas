81e8747675ce57df7f7ef3854264f59ed03e2eee
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-10-16 22:35:51 +0000

    funcion random

--- src/app.js
+++ src/app.js
@@ -14,12 +14,14 @@ window.onload = () => {
     "during my lunch",
     "while I was praying"
   ];
-  // Decloro el string imprimir, en el se guardan todas las partes de la excusa, se usa += para conservar el valor actual y agregar el valor nuevo
-  let Imprimir = "";
-  // En los pasos siguientes se usa un random para generar la posicion de cada arreglo segun la cantidad de lapabras que tenga y se guarda en imprimir
-  Imprimir += who[Math.floor(Math.random() * 4)]; // El arreglo who en la posicion que genere el random se guarda en imprimir
-  Imprimir += " " + action[Math.floor(Math.random() * 4)]; //El arreglo action en la posicion que genere el random se guarda en imprimir
-  Imprimir += " " + what[Math.floor(Math.random() * 3)]; //El arreglo what en la posicion que genere el random se guarda en imprimir
-  Imprimir += " " + when[Math.floor(Math.random() * 5)]; //El arreglo when en la posicion que genere el random se guarda en imprimir
-  document.querySelector("#excuse").innerHTML = Imprimir; // cambia la palabra excuse en el HTML por la excusa generada
+
+  const randVal = numWords => {
+    return Math.floor(Math.random() * numWords);
+  };
+
+  let Imprimir = `${who[randVal(4)]} ${action[randVal(4)]} ${
+    what[randVal(3)]
+  } ${when[randVal(5)]}`;
+
+  document.querySelector("#excuse").innerHTML = Imprimir;
 };
