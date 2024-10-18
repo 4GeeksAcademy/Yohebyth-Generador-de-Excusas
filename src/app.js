95462713a3f683567eecfc1350c0132accaaafb6
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-10-18 21:52:53 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -2,26 +2,23 @@
 import "./style.css";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  //write your code here
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
+//write your code here
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-  const randVal = numWords => {
-    return Math.floor(Math.random() * numWords);
-  };
-
-  let Imprimir = `${who[randVal(4)]} ${action[randVal(4)]} ${
-    what[randVal(3)]
-  } ${when[randVal(5)]}`;
-
-  document.querySelector("#excuse").innerHTML = Imprimir;
+const randVal = numWords => {
+  return Math.floor(Math.random() * numWords);
 };
+
+let Imprimir = `${who[randVal(4)]} ${action[randVal(4)]} 
+  ${what[randVal(3)]} ${when[randVal(5)]}`;
+
+document.querySelector("#excuse").innerHTML = Imprimir;
